f70cdbaf493d4002d15688df424b78ab3f681740
Tyler <whitsett.tyler@gmail.com>
2015-03-20 10:20:44 -0700

    remove date from specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 describe Post do
   it { should validate_presence_of :title }
-  it { should validate_presence_of :date }
+  it { should validate_presence_of :post_text }
 end
