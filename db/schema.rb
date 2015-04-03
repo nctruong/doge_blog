b660259591bd4bff9bab2a7817b1ad31c942988f
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-04-03 15:28:37 -0700

    add user id to comments for permissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327214651) do
+ActiveRecord::Schema.define(version: 20150403215815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150327214651) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
