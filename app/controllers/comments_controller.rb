430b1b79d437b0332c4b59cf93e61fd29ff9beab
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-04-03 16:21:19 -0700

    woops, redirect!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,6 +30,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
     if @comment.update(comment_params)
+      redirect_to post_path(@comment.post)
       flash[:success] = 'Comment Updated!'
     else
       render :edit
