d1cb72a87c7ba1f3e387f4d84ab8593c6674db84
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-04-03 13:35:40 -0700

    add user/comments relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
-  validates :title, :presence => true
-  validates :post_text, :presence => true
+  validates :title, presence: true
+  validates :post_text, presence: true
 end
