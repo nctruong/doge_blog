8e02b3407f02a4cbe84b6b5fe4f7f4c08ce1e107
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-04-03 15:42:56 -0700

    user/comments relationship

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Comment do
   it { should belong_to :post }
+  it { should belong_to :user }
   it { should validate_presence_of :user_name }
   it { should validate_presence_of :comment_text }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe User do
   it { should have_many :posts }
+  it { should have_many :comments}
+
 end
