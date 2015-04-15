d39463fb7e82ec7dcdecf548eec528ffcc66262f
Tyler <whitsett.tyler@gmail.com>
2015-04-15 15:30:14 -0700

    add smaller avatar for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "75x75>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "75x75>", :comment => "35x35>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   # Include default devise modules. Others available are:
