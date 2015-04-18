f3f02262fda8aed805097852d97fa80d5ef5793d
Tyler <whitsett.tyler@gmail.com>
2015-04-17 17:04:57 -0700

    setup polymorphic votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,19 @@
 class VotesController < ApplicationController
+  before_filter :load_voteable
 
   def create
+    @vote = @voteable.votes.new(params[:vote])
+    redirect_to posts_path(@post)
   end
 
   def destroy
+  end
+
+  private
+
+  def load_voteable
+    resource, id = request.path.split('/')[1,2]
+    @voteable = resource.singularize.classify.constantize.find(id)
   end
 
 end
